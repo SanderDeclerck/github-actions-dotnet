982cbd6abb97a918a1692c5d94bc7e78b3c27c57
Declerck, Sander <SDECLE12@volvocars.com>
2019-09-17 12:15:04 +0200

    new console application

---
+++ src/Actions.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Actions.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
